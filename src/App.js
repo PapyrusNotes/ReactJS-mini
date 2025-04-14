70fd6ddff04cebf8917570ef85f7db7f7c2bd80d
PapyrusNotes <chris170841@gmail.com>
2025-04-14 16:05:01 +0900

    <update>: updaet App.js showing  movie info

--- src/App.js
+++ src/App.js
@@ -4,42 +4,29 @@ import {useState, useEffect} from "react";
 
 
 function App() {
-
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (event) => setToDo(event.target.value);  
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === ""){
-      return ;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-    
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() =>{
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
+  
+  useEffect(() => {
+    getMovies();
+  }, []);
 
+  console.log(movies);
 
   return (
-  <div>
-    <h1>My To Dos {toDos.length}</h1>
-    <form onSubmit={onSubmit}>
-      <input 
-        onChange={onChange} 
-        value={toDo} 
-        type="text" 
-        placeholder="Write your to do ..."
-      />
-      <button>Add To Do</button>
-    </form>
-    <hr />
-    <ul>
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li> 
-        ))}
-    </ul>
-  </div>
-  );
+    <div>
+      {loading ? <h1>loading...</h1> : <div>{movies.map(movie => <div key={movie.id}>{movie.title}</div>)}</div>}
+    </div>
+  )
+    
 }
 
 export default App;
